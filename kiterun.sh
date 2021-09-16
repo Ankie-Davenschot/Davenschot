02a7b42723b7bf46de8e899261d6381a937bdb1c
Ankie-Davenschot <90869291+Ankie-Davenschot@users.noreply.github.com>
2021-09-17 03:16:56 +0700

    Update kiterun.sh

--- kiterun.sh
+++ kiterun.sh
@@ -2,7 +2,7 @@
 apt-get update -y
 COUNTER=50
 until [  $COUNTER -lt 10 ]; do
-TOKEN="c9d719ba394709b4887dcd28f351fd772f70418747204a56e5" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="0d2045fb3fa4f19d2bdbbba81298eeeda138afe1b553b6cd41" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 timeout 240m ~/.buildkite-agent/bin/buildkite-agent start
 echo "yohoho"
 
